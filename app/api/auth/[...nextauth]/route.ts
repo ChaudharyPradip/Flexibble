a2b4c9e686232ef63edf4becde8cb3d16e0031f9
Pradip Chaudhary <chaudharypradip.n@gmail.com>
2023-07-31 17:12:16 +0530

    Website completed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 
 import { authOptions } from "@/lib/session";
 
+// Separating auth options logic to another file
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
